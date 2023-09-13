f464a2b1cd5e0dcb6e7bd1384ce4f97f9ee3267b
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-13 11:48:19 +0200

    app module && tsconfig changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+
+// app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+   
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
