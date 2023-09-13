4323af67b60482a7d645a1e44a6c73a14784e55c
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-13 11:49:21 +0200

    test new url from image kit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,37 @@
-  import { BadRequestException, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-  import { AppService } from './app.service';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express/multer';
+import { log } from 'console';
 
-  @Controller()
-  export class AppController {
-    constructor(private readonly appService: AppService) {}
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
 
-    @Get()
-    getHello(): string {
-      return this.appService.getHello();
-    }
-    @Post('upload')
-    @UseInterceptors(FileInterceptor('file'))
-    async uploadImage(@UploadedFile() file) {
-      if (!file) {
-        throw new BadRequestException('No file provided');
-      }
-  
-      const result = await this.appService.uploadImage(
-        file.buffer,
-        file.originalname
-      );
-      return result;
-    }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadImage(@UploadedFile() file) {
+    if (!file) {
+      throw new BadRequestException('No file provided');
+    }
+
+    const result = await this.appService.uploadImage(
+      file.buffer,
+      file.originalname,
+    );
+    console.log('====================================');
+    console.log(result.thumbnailUrl);
+    console.log('====================================');
+    return result;
+  }
+}
