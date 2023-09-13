7a8f66431ae24787dac93593a03905560fe7610e
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-13 11:46:33 +0200

    logic for upload image : -service - controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+  import { BadRequestException, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+  import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express/multer';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Controller()
+  export class AppController {
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    @Get()
+    getHello(): string {
+      return this.appService.getHello();
+    }
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadImage(@UploadedFile() file) {
+      if (!file) {
+        throw new BadRequestException('No file provided');
+      }
+  
+      const result = await this.appService.uploadImage(
+        file.buffer,
+        file.originalname
+      );
+      return result;
+    }
   }
-}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,29 @@
 import { Injectable } from '@nestjs/common';
+const ImageKit = require('imagekit');
 
 @Injectable()
 export class AppService {
+  private imagekit;
+
+  constructor() {
+    this.imagekit = new ImageKit({
+      publicKey: 'public_Kk7vTCZ4Nws9FCCjwussJeGF1vU=',
+      privateKey: 'private_ujQFrwhPcksNYb5gxC7nYqAUXj0=',
+      urlEndpoint: 'https://ik.imagekit.io/azn6jixxj',
+    });
+  }
+  async uploadImage(fileBuffer: Buffer, fileName: string) {
+    try {
+      const response = await this.imagekit.upload({
+        file: fileBuffer,
+        fileName,
+      });
+      return response;
+    } catch (error) {
+      console.error('Image upload failed:', error);
+      throw new Error('Image upload failed: ' + error.message);
+    }
+  }
   getHello(): string {
     return 'Hello World!';
   }
